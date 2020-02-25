d13a7a6207699a0b836be6bf13c9d57904e0f174
Chujie Zeng <chujie.zeng@gmail.com>
2020-02-24 22:03:36 -0500

    88

---
+++ CPP Solutions/88.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+        while (k >= 0) {
+            if (i >= 0 && (j == -1 || nums1[i] >= nums2[j])) {
+                nums1[k--] = nums1[i--];
+            }
+            else if (j >= 0 && (i == -1 || nums1[i] < nums2[j])) {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
